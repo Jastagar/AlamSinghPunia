84c519265bc486894ed0ed9ff81fa61e10bacfaf
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-05-09 23:18:25 +0530

    navbar is responsive

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.set("views", __dirname+"/views")
 app.set("layout", "layouts/layout")
 app.set("router")
 
-
 app.use(expressLayouts)
 app.use(express.static("public"))
 app.use("/", indexRouter)
